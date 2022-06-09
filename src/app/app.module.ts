d5caa1a0dfe9cb80447647287011e467fd4a5f6a
root <root@localhost.localdomain>
2022-06-09 16:42:07 +0200

    ajout page destination

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,7 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppRoutingModule } from './app.routing';
 import { ComponentsModule } from './components/components.module';
+import { DestinationComponent } from './pages/destination/destination/destination.component';
 
 
 @NgModule({
@@ -27,7 +28,8 @@ import { ComponentsModule } from './components/components.module';
   declarations: [
     AppComponent,
     AdminLayoutComponent,
-    AuthLayoutComponent
+    AuthLayoutComponent,
+    DestinationComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
