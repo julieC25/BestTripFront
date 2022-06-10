6d7c068dda4b593424278a54599f20c8aab0288a
root <root@localhost.localdomain>
2022-06-10 16:32:46 +0200

    page afrique

--- src/app/layouts/auth-layout/auth-layout.routing.ts
+++ src/app/layouts/auth-layout/auth-layout.routing.ts
@@ -1,9 +1,20 @@
 import { Routes } from '@angular/router';
+import { AfriqueComponent } from 'src/app/pages/Afrique/afrique/afrique.component';
+import { AmeriqueComponent } from 'src/app/pages/Amerique/amerique/amerique.component';
+import { AsieComponent } from 'src/app/pages/Asie/asie/asie.component';
+import { AustralieComponent } from 'src/app/pages/Australie/australie/australie.component';
+import { EuropeComponent } from 'src/app/pages/Europe/europe/europe.component';
 
 import { LoginComponent } from '../../pages/login/login.component';
 import { RegisterComponent } from '../../pages/register/register.component';
 
 export const AuthLayoutRoutes: Routes = [
     { path: 'login',          component: LoginComponent },
-    { path: 'register',       component: RegisterComponent }
+    { path: 'register',       component: RegisterComponent },
+    { path: 'Afrique',        component: AfriqueComponent},
+    { path: 'Amerique',       component: AmeriqueComponent},
+    { path: 'Asie',           component: AsieComponent},
+    { path: 'Australie',      component: AustralieComponent},
+    { path: 'Europe',         component: EuropeComponent}
+
 ];
