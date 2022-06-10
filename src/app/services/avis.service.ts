09c8524d1309990f6d2bf51cecbc2616ab2bcb52
MahiousI <ishakmahious@yahoo.fr>
2022-06-10 15:02:24 +0200

    ishak: modification mineure du dashboard, et ajout de Avis.services

---
+++ src/app/services/avis.service.ts
@@ -0,0 +1,40 @@
+import { HttpClient, HttpRequest } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs'; // le module rxjs
+import { Avis } from '../model/avis';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class avisService {
+  private baseURL = "http://localhost:9090/avis";
+
+  constructor(private httpClient:HttpClient) { }
+ 
+  public findAll() : Observable<any>{
+    return this.httpClient.get(this.baseURL); 
+  }
+
+  public delete(id:number):Observable<any>{
+    return this.httpClient.delete(this.baseURL+"/"+id); 
+  }
+  
+ /*public save(avis:any):Observable<any>{
+    return this.httpClient.post(this.baseURL,avis);
+  }*/
+  /*enabledString = "false";
+  abonnementNewsletterString = "false";*/
+  /*public save(imageProfil:File,avis:Avis):Observable<any>{
+    if(avis.idAvis){
+      this.idAvisNumber!=0;
+    }
+    const formData=new FormData();
+    formData.append('titre',avis.titre);
+    formData.append('note',avis.note);
+    formData.append('recommandation',avis.recommandation);
+    const requete = new HttpRequest('POST',this.baseURL,formData, 
+      {reportProgress:true,responseType:'text'});
+    return this.httpClient.request(requete);
+  }*/
+
+}
