4c314b85e30864df2c3e700f9da5de0a0b47f75b
tb746 <thom.belle746@gmail.com>
2022-06-17 13:54:09 +0200

    Ajout

--- src/app/services/paysgenerique.service.ts
+++ src/app/services/paysgenerique.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@angular/core';
 import { Lieu } from '../model/lieu';
 import { Pays } from '../model/pays';
 import { Ville } from '../model/ville';
+import {Experience} from '../model/experience';
+import { Guide } from '../model/guide';
 
 @Injectable({
   providedIn: 'root'
@@ -11,5 +13,8 @@ export class PaysgeneriqueService {
  pays: Pays = new Pays();
  ville: Ville = new Ville();
  lieu: Lieu = new Lieu();
+ experience: Experience = new Experience();
+ guide: Guide = new Guide();
+
   constructor() { }
 }
